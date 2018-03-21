b1958a184dc993d8232ed8bb0103035cd96b51b5
Minkyu Jung <hestati@kaist.ac.kr>
2018-03-21 11:14:40 +0900

    Refactoring and Cleanup PGP.cc

--- src/PGP.cc
+++ src/PGP.cc
@@ -1,109 +1,154 @@
 #include "PGP.hh"
 #include <unistd.h>
 #include <string.h>
-#include <iostream>
+#include <fstream>
+#include <streambuf>
 
-gpgme_error_t passphrase_cb(void *pass, const char *uid_hint,
-    const char *passphrase_info, int last_was_bad, int fd){
-	gpgme_io_writen(fd, (char*)pass, strlen((char*)pass));
-	gpgme_io_writen(fd, "\n", 1);
-}
+namespace PGP {
+    gpgme_error_t passphrase_cb(void *pass, const char *uid_hint,
+            const char *passphrase_info, int last_was_bad, int fd) {
+        gpgme_io_writen(fd, (char*)pass, strlen((char*)pass));
+        gpgme_io_writen(fd, "\n", 1);
+        return GPG_ERR_NO_ERROR;
+    }
+
+    gpgme_error_t get_passphrase_info_cb(void *pass, const char *uid_hint,
+           const char *passphrase_info, int last_was_bad, int fd) {
+        auto pgp = static_cast<PGP *>(pass);
+        cout << "cb!" << endl;
+        cout << uid_hint << endl;
+        cout << passphrase_info << endl;
+        exit(0);
+        return GPG_ERR_NO_ERROR;
+    }
+
+    void __attribute((constructor)) initGPG() {
+        gpg_error_t err;
+        gpgme_check_version (NULL);
+        setlocale (LC_ALL, "");
+        gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
+        err = gpgme_engine_check_version (GPGME_PROTOCOL_OpenPGP);
+        fail_if_err (err);
+    }
+
+    void PGP::get_passphrase_info() {
+        gpgme_set_passphrase_cb(ctx, get_passphrase_info_cb, (void *)this);
+        this->Decrypt("A");
+    }
+
+
+    void PGP::InitPrikey(string prikey) {
+        ifstream pri(prikey);
+
+        priArmored.assign((istreambuf_iterator<char>(pri)),
+                    std::istreambuf_iterator<char>());
+    }
+
+    void PGP::InitPubkey(string pubkey) {
+        ifstream pub(pubkey);
+        pubArmored.assign((istreambuf_iterator<char>(pub)),
+                    std::istreambuf_iterator<char>());
+    }
+
+    void PGP::InitCTX() {
+        gpgme_error_t err;
+        gpgme_data_t keydata;
+        gpgme_import_result_t import_result;
+
+        err = gpgme_new(&ctx);
+        fail_if_err(err);
+        gpgme_set_pinentry_mode(ctx, GPGME_PINENTRY_MODE_LOOPBACK);
+        gpgme_set_armor(ctx, 1);
+        fail_if_err(err);
+
+        err = gpgme_data_new_from_mem(&keydata, priArmored.c_str(), priArmored.size(), 0);
+        fail_if_err(err);
+
+        err = gpgme_op_import(ctx, keydata);
+        fail_if_err(err);
+
+        import_result = gpgme_op_import_result(ctx);
+        err = gpgme_get_key(ctx, import_result -> imports -> fpr, &prikey, 1);
+        fail_if_err(err);
+        get_passphrase_info();
+    }
+
+    // This provide enc, dec method
+    PGP::PGP(string pubkey, string prikey) {
+        InitPubkey(pubkey);
+        InitPrikey(prikey);
+        InitCTX();
+    }
+
+    // This only provide enc
+    PGP::PGP(string pubkey) {
+        InitPubkey(pubkey);
+        InitCTX();
+    }
+
 
 bool PGP::Verify_Pass(){
-  FILE* pub_fp = fopen("./pubkey", "r");
-  fseek(pub_fp, 0, SEEK_END);
-  int pub_size = ftell(pub_fp);
-  rewind(pub_fp);
-  char* key = (char*) malloc(pub_size + 1);
-  fread(key, pub_size, 1, pub_fp);
-  fclose(pub_fp);
-	char* pt = "This is plain text\n";
-	char* ct = this->Encrypt(pt, key);
-	char* dt = this->Decrypt(ct);
-	
-	return !strcmp(pt, dt);
+    char* pt = "This is plain text\n";
+    char* ct = this->Encrypt(pt);
+    char* dt = this->Decrypt(ct);
+    return !strcmp(pt, dt);
 }
-	
+
+    /*
 PGP::PGP(char* prikey_path){
-  gpgme_error_t err;
-	gpgme_data_t keydata;
-	gpgme_import_result_t import_result;
-  
-	gpgme_check_version (NULL);
-  setlocale (LC_ALL, "");
-  gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
-  err = gpgme_engine_check_version (GPGME_PROTOCOL_OpenPGP);
-  fail_if_err (err);
+    passphrase = getpass("Enter passphrase: ");
+    gpgme_set_passphrase_cb(ctx, passphrase_cb, passphrase);
 
-	err = gpgme_new(&ctx);
-  fail_if_err(err);
-  gpgme_set_pinentry_mode(ctx, GPGME_PINENTRY_MODE_LOOPBACK);
-  gpgme_set_armor(ctx, 1);
-  fail_if_err(err);
-  
-	passphrase = getpass("Enter passphrase: ");
-  gpgme_set_passphrase_cb(ctx, passphrase_cb, passphrase);
 
-  err = gpgme_data_new_from_file(&keydata, prikey_path, 1);
-  fail_if_err(err);
-
-	err = gpgme_op_import(ctx, keydata);
-  fail_if_err(err);
-
-	import_result = gpgme_op_import_result(ctx);
-	err = gpgme_get_key(ctx, import_result -> imports -> fpr, &prikey, 1);
-  fail_if_err(err);
-
-	while(!this->Verify_Pass()){
-		std::cout << "Wrong Passphrase!" << std::endl;
-		passphrase = getpass("Enter passphrase: ");
-		gpgme_set_passphrase_cb(ctx, passphrase_cb, passphrase);
-	}
-	
+    while(!this->Verify_Pass()){
+        std::cout << "Wrong Passphrase!" << std::endl;
+        passphrase = getpass("Enter passphrase: ");
+        gpgme_set_passphrase_cb(ctx, passphrase_cb, passphrase);
+    }
 }
+    */
 
 char* PGP::Decrypt(char* ct){
-  gpgme_data_t cipher_text, plain_text;
-  gpgme_data_t keydata;
-  gpgme_error_t err;
-	gpgme_passphrase_cb_t f;
-	void* hook;
-	gpgme_get_passphrase_cb(ctx, &f, &hook);
+    gpgme_data_t cipher_text, plain_text;
+    gpgme_passphrase_cb_t f;
+    void* hook;
+    gpgme_get_passphrase_cb(ctx, &f, &hook);
 
-  gpgme_data_new_from_mem(&cipher_text, ct, strlen(ct), 1);
-	gpgme_data_new(&plain_text);
-  gpgme_op_decrypt(ctx, cipher_text, plain_text);
+    gpgme_data_new_from_mem(&cipher_text, ct, strlen(ct), 1);
+    gpgme_data_new(&plain_text);
+    gpgme_op_decrypt(ctx, cipher_text, plain_text);
 
-	size_t size;
-	char* result = gpgme_data_release_and_get_mem(plain_text, &size);
-  return result;
+    size_t size;
+    char* result = gpgme_data_release_and_get_mem(plain_text, &size);
+    return result;
 }
 
-char* PGP::Encrypt(char* pt, char* pubkey){
-  gpgme_error_t err;
-  gpgme_data_t keydata;
-  gpgme_data_t cipher_text, plain_text;
-  gpgme_import_result_t import_result;
-  gpgme_key_t key[2] = {NULL, NULL};
+char* PGP::Encrypt(char* pt){
+    gpgme_error_t err;
+    gpgme_data_t keydata;
+    gpgme_data_t cipher_text, plain_text;
+    gpgme_import_result_t import_result;
+    gpgme_key_t key[2] = {NULL, NULL};
 
-  err = gpgme_data_new_from_mem(&keydata, pubkey, strlen(pubkey), 1);
-  fail_if_err(err);
+    err = gpgme_data_new_from_mem(&keydata, pubArmored.c_str(), pubArmored.size(), 0);
+    fail_if_err(err);
 
-  err = gpgme_op_import(ctx, keydata);
-  fail_if_err(err);
+    err = gpgme_op_import(ctx, keydata);
+    fail_if_err(err);
 
-  import_result = gpgme_op_import_result(ctx);
-  err = gpgme_get_key(ctx, import_result->imports->fpr, &key[0], 0);
-  fail_if_err(err);
+    import_result = gpgme_op_import_result(ctx);
+    err = gpgme_get_key(ctx, import_result->imports->fpr, &key[0], 0);
+    fail_if_err(err);
 
-  err = gpgme_data_new_from_mem(&plain_text, pt, strlen(pt), 0);
-  fail_if_err(err);
+    err = gpgme_data_new_from_mem(&plain_text, pt, strlen(pt), 0);
+    fail_if_err(err);
 
-	gpgme_data_new(&cipher_text);
-  err = gpgme_op_encrypt(ctx, key, GPGME_ENCRYPT_ALWAYS_TRUST, plain_text, cipher_text);
-  fail_if_err(err);
+    gpgme_data_new(&cipher_text);
+    err = gpgme_op_encrypt(ctx, key, GPGME_ENCRYPT_ALWAYS_TRUST, plain_text, cipher_text);
+    fail_if_err(err);
 
-	size_t size;
-	char* result = gpgme_data_release_and_get_mem(cipher_text, &size);
-  return result;
+    size_t size;
+    char* result = gpgme_data_release_and_get_mem(cipher_text, &size);
+    return result;
+}
 }
--- src/PGP.hh
+++ src/PGP.hh
@@ -1,32 +1,44 @@
 #ifndef __PGP__
 #define __PGP__
+#include <iostream>
+#include <string>
 #include <stdlib.h>
 #include <gpgme.h>
 #include <locale.h>
 #include <errno.h>
 
-#define fail_if_err(err)          \
-  do                \
-    {               \
-      if (err)              \
-        {             \
-          fprintf (stderr, "%s:%d: %s: %s\n",     \
-                   __FILE__, __LINE__, gpgme_strsource (err), \
-       gpgme_strerror (err));     \
-          exit (1);           \
-        }             \
-    }               \
-  while (0)
+using namespace std;
 
-class PGP{
-	private:
-		gpgme_ctx_t ctx;
-		gpgme_key_t prikey;
-		char* passphrase;
-	public:
-		PGP(char* prikey_path);
-		char* Encrypt(char* pt, char* pubkey);
-		char* Decrypt(char* ct);
-		bool Verify_Pass();
-};
+namespace PGP {
+    #define fail_if_err(err) do {\
+        if (err) { \
+            cerr << __FILE__ << ":" \
+            << __LINE__ << ": " \
+            << gpgme_strsource (err) << ": " \
+            << gpgme_strerror (err) << endl; \
+            exit(1); \
+        } \
+    } while(0);
+
+    class PGP
+    {
+        private:
+            string pubArmored;
+            string priArmored;
+
+            gpgme_ctx_t ctx;
+            gpgme_key_t prikey;
+            char* passphrase;
+            void get_passphrase_info();
+            void InitPrikey(string prikey);
+            void InitPubkey(string pubkey);
+            void InitCTX();
+        public:
+            PGP(string pubkey, string prikey);
+            PGP(string pubkey);
+            char* Encrypt(char* pt);
+            char* Decrypt(char* ct);
+            bool Verify_Pass();
+    };
+}
 #endif
