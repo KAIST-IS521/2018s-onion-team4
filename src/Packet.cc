37e782b06f9e15ea768e6036517cc2ad114200f7
Minkyu Jung <hestati@kaist.ac.kr>
2018-03-22 16:45:50 +0900

    Update Packet class to work with SelectServer
    - This use asyncronous method to build a packet.
    - Only `Msg class` is implemented.

--- src/Packet.cc
+++ src/Packet.cc
@@ -3,63 +3,104 @@
 #include <string.h>
 #include <stdio.h>
 #include "Packet.hh"
+namespace Packet {
 
-#define MSG 1
-#define HANDSHAKE 0
 
-char* HandShake::Serialize() {
-    size_t lth = 13 + this->id_length + this->pubkey_length +
-                  sizeof(uint32_t) * this->connected_nodes;
-
-    char* packet = (char *) malloc(lth);
-    
-    *(uint8_t *) (packet + 0) = HANDSHAKE;
-
-    *(uint32_t *) (packet + 1) = htonl(this->id_length);
-    if(this->id)
-        memcpy(packet + 5, this->id, this->id_length);
-
-    *(uint32_t *) (packet + 5 + this->id_length) = htonl(this->pubkey_length);
-    if(this->pubkey)
-        memcpy(packet + 9 + this->id_length, this->pubkey, this->pubkey_length);
-
-    *(uint32_t *) (packet + 9 + this->id_length + this->pubkey_length) 
-                                              = htonl(this->connected_nodes);
-    if(this->node_ips){
-        for(unsigned int i = 0; i < this->connected_nodes; i++){
-            *(uint32_t *) (packet + 13 + sizeof(uint32_t) * i + this->id_length + this->pubkey_length)
-                 = htonl(this->node_ips[i]);
+    Packet* Unserialize(ReadCTX *ctx) {
+        Packet *packet;
+        int type;
+        if (!ctx->aux) {
+            CTXRead(ctx, (char *)&type, 1);
+            if (type == HANDSHAKE) {
+                packet = new HandShake();
+            } else if(type == MSG) {
+                packet = new Msg();
+            } else {
+                return NULL;
+            }
+        } else {
+            packet = static_cast<Packet *>(ctx->aux);
         }
+        packet->ContinueBuild(ctx);
+        return packet;
     }
-    return packet;
-}
 
-char* Msg::Serialize() {
-    size_t lth = 9 + this->length;
+    char* Msg::Serialize() {
+        size_t lth = 5 + this->length;
 
-    char* packet = (char *) malloc(lth);
-    
-    *(uint8_t *) (packet + 0) = MSG;
-    *(uint32_t *) (packet + 1) = htonl(this->timestamp);
-    *(uint32_t *) (packet + 5) = htonl(this->length);
-    if(this->message)
-        memcpy(packet + 9, this->message, this->length);
+        char* packet = (char *) malloc(lth);
 
-    return packet;
-}
+        *(uint8_t *) (packet + 0) = MSG;
+        *(uint32_t *) (packet + 1) = htonl(this->length);
+        if(this->message)
+            memcpy(packet + 5, this->message, this->length);
 
-Packet* Deserialize(char* recv) {
-    uint8_t packet_type = *(uint8_t *)(recv + 0);
-    if( packet_type == MSG ){
-        Msg* msg = new Msg();
-        
-        msg->timestamp = ntohl(*(uint32_t *) (recv + 1));
-        msg->length = ntohl(*(uint32_t *) (recv + 5));
-        msg->message = (char *) calloc(1, sizeof(msg->length));
-        memcpy(msg->message, recv + 9, msg->length);
-        
-        return msg;
-    }else if ( packet_type == HANDSHAKE ) {
+        return packet;
+    }
+
+    void Msg::ContinueBuild(ReadCTX *ctx) {
+        int l;
+        // state for parse length
+        if (state == 0 && CTXGetsz(ctx) >= 4) {
+            CTXRead(ctx, (char *)&l, 4);
+            length = ntohl(l);
+            state = 1;
+        }
+        // state for parse message
+        if (state == 1 && CTXGetsz(ctx) >= length) {
+            message = (char *)calloc(1, length+1);
+            CTXRead(ctx, message, length);
+            // Release unused buffer
+            CTXDiscard(ctx);
+            setReady();
+            ctx->aux = NULL;
+            return;
+        }
+        ctx->aux = this;
+    }
+
+    char *Msg::GetMessage(void) {
+        return message;
+    }
+
+    int Msg::GetMessageLength(void) {
+        return length;
+    }
+
+    Msg::~Msg(void) {
+        if (message) free(message);
+    }
+
+    char * HandShake::Serialize(void) {
+        size_t lth = 13 + this->id_length + this->pubkey_length +
+                      sizeof(uint32_t) * this->connected_nodes;
+
+        char* packet = (char *) malloc(lth);
+
+        *(uint8_t *) (packet + 0) = HANDSHAKE;
+
+        *(uint32_t *) (packet + 1) = htonl(this->id_length);
+        if(this->id)
+            memcpy(packet + 5, this->id, this->id_length);
+
+        *(uint32_t *) (packet + 5 + this->id_length) = htonl(this->pubkey_length);
+        if(this->pubkey)
+            memcpy(packet + 9 + this->id_length, this->pubkey, this->pubkey_length);
+
+        *(uint32_t *) (packet + 9 + this->id_length + this->pubkey_length)
+                                                  = htonl(this->connected_nodes);
+        if(this->node_ips){
+            for(unsigned int i = 0; i < this->connected_nodes; i++){
+                *(uint32_t *) (packet + 13 + sizeof(uint32_t) * i + this->id_length + this->pubkey_length)
+                     = htonl(this->node_ips[i]);
+            }
+        }
+        return packet;
+    }
+
+    void HandShake::ContinueBuild(ReadCTX *ctx) {
+        // TODO: Fill me
+        /*
         HandShake* hs = new HandShake();
 
         hs->id_length = ntohl(*(uint32_t *) (recv + 1));
@@ -71,17 +112,13 @@ Packet* Deserialize(char* recv) {
         memcpy(hs->pubkey, recv + 9 + hs->id_length, hs->pubkey_length);
 
         hs->connected_nodes = ntohl(*(uint32_t *) (recv + 9 + hs->id_length +
-                                    hs->pubkey_length));
+                    hs->pubkey_length));
         hs->node_ips = (uint32_t *) calloc(hs->connected_nodes, sizeof(uint32_t));
         for(unsigned int i = 0; i < hs->connected_nodes; i++){
-            uint32_t temp = ntohl(*(uint32_t *) 
-                            (recv + 13 + 4 * i + hs->id_length + hs->pubkey_length));
+            uint32_t temp = ntohl(*(uint32_t *)
+                    (recv + 13 + 4 * i + hs->id_length + hs->pubkey_length));
             hs->node_ips[i] = temp;
         }
-
-        return hs;
-    }else{
-        return 0;
+        */
     }
 }
-
--- src/Packet.hh
+++ src/Packet.hh
@@ -1,41 +1,57 @@
 #ifndef __PACKET__
 #define __PACKET__
 
-#include <stdint.h>
+#include <cstdint>
+#include "SelectServer/src/context.h"
 
-class Packet
-{
-    public:
-        Packet() {}
-        ~Packet() {}
-        virtual char *Serialize() = 0;
-};
+#define HANDSHAKE 0
+#define MSG 1
+namespace Packet {
+    class Packet
+    {
+        private:
+            bool ready;
+            int type;
+        public:
+            Packet(int t) : ready(false), type(t) { };
+            ~Packet() {}
+            virtual char *Serialize() = 0;
+            virtual void ContinueBuild(ReadCTX *ctx) = 0;
+            int getType() { return type; };
+            int isReady() { return ready; };
+            void setReady() { ready = true; };
 
-class HandShake : public Packet
-{
-    public:
-        uint32_t id_length, pubkey_length, connected_nodes;
-        char* id, *pubkey;
-        uint32_t* node_ips;
-        
-        char *Serialize();
+    };
 
-        HandShake() : Packet() { }
-        ~HandShake() { }
-};
+    class HandShake : public Packet
+    {
+        public:
+            uint32_t id_length, pubkey_length, connected_nodes;
+            char* id, *pubkey;
+            uint32_t* node_ips;
 
-class Msg : public Packet
-{
-    public:
-        uint32_t timestamp, length;
-        char* message;
+            char *Serialize();
+            void ContinueBuild(ReadCTX *ctx);
 
-        char *Serialize();
+            HandShake() : Packet(HANDSHAKE) { }
+            ~HandShake() { }
+    };
 
-        Msg() : Packet() { }
-        ~Msg() { }
-};
+    class Msg : public Packet
+    {
+        private:
+            int state = 0;
+            uint32_t length;
+            char *message = NULL;
+        public:
+            char *Serialize(void);
+            char *GetMessage(void);
+            int GetMessageLength(void);
+            void ContinueBuild(ReadCTX *ctx);
+            Msg() : Packet(MSG) {};
+            ~Msg();
+    };
 
-Packet* Deserialize(char*);
-
+    Packet *Unserialize(ReadCTX *ctx);
+}
 #endif
