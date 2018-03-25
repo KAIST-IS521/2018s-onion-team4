91469614327983762ae8d477130080691cb34f98
saykim0727 <saykim0727@kaist.ac.kr>
2018-03-26 02:07:21 +0900

    Update DOCKER
    - Update README.md
    - Update docker file
    - update shell file

--- Docker/team4.sh
+++ Docker/team4.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 #source ./test.sh
 
-git clone -b proto https://github.com/KAIST-IS521/2018s-onion-team4.git
+git clone -b proto http://github.com/KAIST-IS521/2018s-onion-team4.git
 cd 2018s-onion-team4
 make
+mkdir /bin/go
+go get github.com/zyxar/image2ascii
 ./OnionMessenger --priv $PRIV --pub $PUB --node $NODE
