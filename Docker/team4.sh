8f1f294d3b9455add84dd6f74a0b900fc9516acb
saykim0727 <saykim0727@kaist.ac.kr>
2018-03-26 20:01:47 +0900

    Update ascii code

--- Docker/team4.sh
+++ Docker/team4.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 #source ./test.sh
-
 git clone -b proto http://github.com/KAIST-IS521/2018s-onion-team4.git
 cd 2018s-onion-team4
 make
 mkdir /bin/go
-go get github.com/zyxar/image2ascii
+go get github.com/stdupp/goasciiart
+echo $PRIV;
 if [ $NODE ];
-  then ./OnionMessenger --priv $PRIV --pub $PUB --node $NODE
-  else ./OnionMessenger --priv $PRIV --pub $PUB
+  then ./OnionMessenger --priv $PRIV --pub $PUB --node $NODE -p $PORT
+  else ./OnionMessenger --priv $PRIV --pub $PUB -p $PORT
 fi
